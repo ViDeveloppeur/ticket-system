d11e6d54ed84bf6d851fba335d0052cb884bad6f
Vi <62430555+ViVeesio@users.noreply.github.com>
2020-07-19 20:47:59 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 
 module.exports = async (bot) => {
 
-    console.log(`${bot.user.username} is ready !`);
+    console.log(`${bot.user.username} est prêt !`);
 
 } 
