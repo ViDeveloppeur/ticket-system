990e9595efa4e47821890884cd5266d678f274e8
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-05-20 11:45:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const config = require("./Storage/config.json");
 
 const bot = new Discord.Client({
-    disableEveryone: true,
-    autoReconnect: true,
-    disabledEvents: ["TYPING_START"],
-    partials: ['MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'REACTION']
+  disableEveryone: true,
+  autoReconnect: true,
+  disabledEvents: ["TYPING_START"],
+  partials: ["MESSAGE", "CHANNEL", "GUILD_MEMBER", "REACTION"],
 });
 
 bot.commands = new Discord.Collection();
@@ -16,9 +16,9 @@ const loadCommands = require("./functions/commands.js");
 const loadEvents = require("./functions/events.js");
 
 const load = async () => {
-    await loadCommands.run(bot);
-    await loadEvents.run(bot);
-}
+  await loadCommands.run(bot);
+  await loadEvents.run(bot);
+};
 
 load();
 bot.login(config.token);
